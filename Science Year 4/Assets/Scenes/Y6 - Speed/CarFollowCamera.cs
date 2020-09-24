d6f85c084f133ce7d5db69b8a92e476fc48213e6
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2020-09-24 17:29:38 +0800

    Updated 24/09/20 on Windows

---
+++ Science Year 4/Assets/Scenes/Y6 - Speed/CarFollowCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarFollowCamera : MonoBehaviour
+{
+    public Transform objToFollow;
+    public Vector3 offset;
+    public float followSpeed = 10f;
+    public float lookSpeed = 10f;
+
+    public void LookAtTarget()
+    {
+        Vector3 _lookDir = objToFollow.position - transform.position;
+        Quaternion _rot = Quaternion.LookRotation(_lookDir, Vector3.up);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 _targetPos = objToFollow.position + objToFollow.forward * offset.z +
+                objToFollow.right * offset.x +
+                objToFollow.up * offset.y;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+}
---
+++ Science Year 4/Assets/Scenes/Y6 - Speed/CarMovemtnController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovemtnController : MonoBehaviour
+{
+    private float m_horizontalInput;
+    private float m_verticalInput;
+    private float m_steeringAngle;
+    private float m_brake;
+
+    public WheelCollider rightFrontW, leftFrontW;
+    public WheelCollider rightBackW, leftBackW;
+
+    public Transform rightFrontT, leftFrontT;
+    public Transform rightBackT, leftBackT;
+
+    public float maxSteerAngle;
+    public float motorForce;
+    public float brakeForce;
+
+    bool isBreaking;
+
+    public void GetInput()
+    {
+        m_horizontalInput = Input.GetAxis("Horizontal");
+        m_verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        m_steeringAngle = maxSteerAngle * m_horizontalInput;
+
+        rightFrontW.steerAngle = m_steeringAngle;
+        leftFrontW.steerAngle = m_steeringAngle;
+    }
+
+    public void Accelerate()
+    {
+        rightFrontW.motorTorque = 1f * motorForce; // joystick or button
+        leftFrontW.motorTorque = 1f * motorForce;
+    }
+
+    private void Brake()
+    {
+        rightFrontW.brakeTorque = brakeForce;
+        leftFrontW.brakeTorque = brakeForce;
+    }
+
+    private void UpdateWheelPos(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void UpdateWHeelPoses()
+    {
+        UpdateWheelPos(rightFrontW, rightFrontT);
+        UpdateWheelPos(leftFrontW, leftFrontT);
+        UpdateWheelPos(rightBackW, rightBackT);
+        UpdateWheelPos(leftBackW, leftBackT);
+    }
+
+    private void FixedUpdate()
+    {
+        //brakeForce = 0f;
+        GetInput();
+        Steer();
+        UpdateWHeelPoses();
+        Accelerate();
+        Brake();
+
+      
+    }
+}
