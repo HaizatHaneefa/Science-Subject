d21992645728a019deb8570c9ff15ab79fc51768
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2020-11-10 20:03:05 +0800

    Updated Project 10/11/20 on iOS
    - updated maze
    - updated scene manager for eclipse game
    - some other shit i did but i forgot what it was

--- Science Year 4/Assets/EclipseCollider.cs
+++ Science Year 4/Assets/EclipseCollider.cs
@@ -12,18 +12,49 @@ public class EclipseCollider : MonoBehaviour
         manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<EclipseGameManager>();
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            manager.confirmButton.gameObject.SetActive(true);
+            if (gameObject.CompareTag("Animal 1") && !manager.isDone[0])
+            {
+                // prompt the thingy
+                manager.confirmButton.gameObject.SetActive(true);
+            }
+            else if (gameObject.CompareTag("Animal 2") && !manager.isDone[0])
+            {
+                // prompt the other thingy
+                manager.wrongPop.SetActive(true);
+            }
+            else if (gameObject.CompareTag("Animal 3") && !manager.isDone[0])
+            {
+                manager.wrongPop.SetActive(true);
+                // prompt the same other thingy
+            }
+
+            if (gameObject.CompareTag("Animal 2") && manager.isDone[0])
+            {
+                // prompt the thingy
+                manager.confirmButton.gameObject.SetActive(true);
+            }
+            else if (gameObject.CompareTag("Animal 3") && manager.isDone[0] && !manager.isDone[1])
+            {
+                // prompt the other thingy
+                manager.wrongPop.SetActive(true);
+            }
+
+            if (gameObject.CompareTag("Animal 3") && manager.isDone[1])
+            {
+                // prompt the thingy
+                manager.confirmButton.gameObject.SetActive(true);
+            }
         }
-        // pop a button
     }
 
     private void OnTriggerExit(Collider other)
     {
         manager.confirmButton.gameObject.SetActive(false);
+        manager.wrongPop.SetActive(false);
     }
 
     void Update()
--- Science Year 4/Assets/EclipseGameManager.cs
+++ Science Year 4/Assets/EclipseGameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class EclipseGameManager : MonoBehaviour
 {
@@ -11,25 +12,53 @@ public class EclipseGameManager : MonoBehaviour
 
     [SerializeField] private GameObject[] col;
     [SerializeField] private TextMeshProUGUI progresstext;
-    [SerializeField] private GameObject popInfo;
+    [SerializeField] public GameObject popInfo, wrongPop, endpop, pausePop, introPop;
     int cur;
 
     void Start()
     {
         popInfo.SetActive(false);
+        wrongPop.SetActive(false);
+        endpop.SetActive(false);
+        pausePop.SetActive(false);
+        introPop.SetActive(true);
 
         isDone = new bool[3];
         confirmButton.gameObject.SetActive(false);
+        Time.timeScale = 1;
     }
 
     void Update()
     {
         progresstext.text = cur.ToString() + "/3";
+
+        if (!isDone[0])
+        {
+            wrongPop.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Please visit stage 1";
+            popInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "stage 1";
+        }
+        else if (isDone[0])
+        {
+            wrongPop.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Please visit stage 2";
+            popInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "stage 2";
+        }
+
+        if (isDone[1])
+        {
+            popInfo.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "stage 3";
+            Debug.Log("ww");
+        }
+
+        if (isDone[2])
+        {
+            endpop.SetActive(true);
+        }
     }
 
     public void Confirm()
     {
         popInfo.SetActive(true);
+        confirmButton.gameObject.SetActive(false);
     }
 
     public void Continue()
@@ -43,18 +72,50 @@ public class EclipseGameManager : MonoBehaviour
             isDone[0] = true;
 
             col[0].transform.GetChild(0).gameObject.SetActive(false);
+            col[0].GetComponent<BoxCollider>().enabled = false;
         }
         else if (isDone[0] && !isDone[1])
         {
             isDone[1] = true;
 
             col[1].transform.GetChild(0).gameObject.SetActive(false);
+            col[1].GetComponent<BoxCollider>().enabled = false;
         }
         else if (isDone[1] && !isDone[2])
         {
             isDone[2] = true;
 
             col[2].transform.GetChild(0).gameObject.SetActive(false);
+            col[2].GetComponent<BoxCollider>().enabled = false;
         }
+    }
+
+    public void _Pause(int index)
+    {
+        if (index == 0)
+        {
+            pausePop.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else if (index == 1)
+        {
+            pausePop.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void _Start()
+    {
+        introPop.SetActive(false);
+    }
+
+    public void _BacktoMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void _Retry()
+    {
+        SceneManager.LoadScene("Y6 - Eclipse Game");
     }
 }
