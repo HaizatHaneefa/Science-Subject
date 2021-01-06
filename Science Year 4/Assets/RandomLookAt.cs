52850cf7ab2de8bf311ee37973263bb3e80617b4
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2021-01-06 12:03:55 +0800

    Updated Project 6/1/21

---
+++ Science Year 4/Assets/RandomLookAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLookAt : MonoBehaviour
+{
+    public Transform cam;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+}
--- Science Year 4/Assets/Scripts/FractionQuizManager.cs
+++ Science Year 4/Assets/Scripts/FractionQuizManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class FractionQuizManager : MonoBehaviour
 {
+    // ---------------------------- Variables ------------------------------ //
     [SerializeField] private GameObject[] questions;
     [SerializeField] private GameObject endpop;
     [SerializeField] private AudioSource aSource;
