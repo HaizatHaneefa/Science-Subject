b27040eff2320619905deca64261d283d744a653
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2021-02-23 17:08:54 +0800

    Updated Project 23/2/21

---
+++ Science Year 4/Assets/randomcharactercontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomcharactercontroller : MonoBehaviour
+{
+    CharacterController charcontroller;
+    float vertical, horizontal;
+
+    void Start()
+    {
+        charcontroller = GetComponent<CharacterController>();   
+    }
+
+    void Movement()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+
+
+    }
+    void FixedUpdate()
+    {
+        Movement();
+    }
+}
