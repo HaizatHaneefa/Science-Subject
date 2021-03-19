bfa936333c1f6e804db60ebee8590cafff803dd9
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2021-03-19 16:38:44 +0800

    Updated Project 18/3/21

--- Science Year 4/Assets/MoneyGameManager.cs
+++ Science Year 4/Assets/MoneyGameManager.cs
@@ -8,29 +8,40 @@ public class MoneyGameManager : MonoBehaviour
 {
     [SerializeField] private MoneyList[] moneylist;
 
-    [SerializeField] private float initValue, changeValue, totalChangeValue;
+    [SerializeField] private float changeValue, totalChangeValue, timer;
     [SerializeField] private Sprite[] moneySprite;
-    [SerializeField] private TextMeshProUGUI dialogueText, changeText;
+    [SerializeField] private TextMeshProUGUI dialogueText, changeText, timerText;
     [SerializeField] private string[] dialogue;
+
+    [SerializeField] private Image itemImage;
+    [SerializeField] private GameObject yayPop;
 
     int cur;
 
     void Start()
     {
         dialogue = new string[3];
-        dialogue[0] = "I would like to buy this ";
+        dialogue[0] = "I would like to buy ";
         dialogue[1] = ". It costs ";
         dialogue[2] = "I'll give you RM 1. You owe me ";
 
-        initValue = 1f;
-
         cur = Random.Range(0, moneylist.Length);
+        yayPop.SetActive(false);
+
+        timer = 60f;
+
+        itemImage.enabled = false;
+    }
+
+    public void StartGame()
+    {
+        itemImage.enabled = true;
+
+        // stuff starts here
     }
 
     public void _Value(int index)
     {
-        changeText.text = totalChangeValue.ToString();
-
         if (index == 0)
         {
             totalChangeValue += .50f;
@@ -51,18 +62,21 @@ public class MoneyGameManager : MonoBehaviour
         {
             totalChangeValue += .01f;
         }
+
+        changeText.text = totalChangeValue.ToString();
     }
 
     public void _Calculate()
     {
         if (totalChangeValue == moneylist[cur].change)
         {
+            ChangeQuestion();
             Debug.Log("qwqwewew");
         }
         else if (totalChangeValue != moneylist[cur].change)
         {
             // redo it
-            ChangeQuestion();
+            totalChangeValue = 0f;
         }
     }
 
@@ -73,21 +87,36 @@ public class MoneyGameManager : MonoBehaviour
 
     IEnumerator Change()
     {
+        yayPop.SetActive(true);
+        yayPop.GetComponent<Animation>().Play("SuccessPop");
+        // do some animation shit
+
         // display yay
         // sound
         totalChangeValue = 0f;
 
         yield return new WaitForSeconds(1f);
 
+        cur = Random.Range(0, moneylist.Length);
 
         changeText.text = totalChangeValue.ToString();
+
+        yayPop.SetActive(false);
         // close yay
         // change question
     }
 
     void Update()
     {
+        itemImage.sprite = moneylist[cur].artwork;
 
-        dialogueText.text = dialogue[0] + moneylist[cur].name + dialogue[1] + moneylist[cur].value + "sens. " + dialogue[2] + moneylist[cur].change + "sens";
+        dialogueText.text = dialogue[0] + moneylist[cur].name + dialogue[1] + moneylist[cur].value + "sen. " + dialogue[2] + moneylist[cur].change + "sen";
+
+        int minutes = Mathf.FloorToInt(timer / 60F);
+        int seconds = Mathf.FloorToInt(timer - minutes * 60);
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timer -= Time.deltaTime;
+        timerText.text = niceTime;
     }
 }
