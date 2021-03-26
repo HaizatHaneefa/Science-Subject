b6019da8f21cd231620699a8878e38ce8a76d5d9
Haizat Haneefa <haizathaneefa.hh@gmail.com>
2021-03-26 17:42:19 +0800

    Updated Project 26/3/21

--- Science Year 4/Assets/TimeGameManager.cs
+++ Science Year 4/Assets/TimeGameManager.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class TimeGameManager : MonoBehaviour
 {
-    [SerializeField] private float timer;
-    [SerializeField] private int score, answerInt, monthInt;
+    [SerializeField] private TimeQuestions[] questions;
 
-    [SerializeField] private TextMeshProUGUI scoreText, timerText;
+    [SerializeField] private float timer;
+    [SerializeField] private int score, answerInt;
+
+    [SerializeField] private TextMeshProUGUI scoreText, timerText, questionsText;
+    [SerializeField] private GameObject introPop, yayPop, endPop;
+
     bool hasStarted;
+
+    int cur;
 
     void Start()
     {
-        timer = 60f;
+        timer = 3f;
+        timerText.text = "0";
 
-        monthInt = 30;
+        introPop.SetActive(true);
+        yayPop.SetActive(false);
+        endPop.SetActive(false);
     }
 
     public void _Button()
@@ -24,15 +34,67 @@ public class TimeGameManager : MonoBehaviour
         GameObject go = EventSystem.current.currentSelectedGameObject;
         answerInt = int.Parse(go.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text);
 
-        //if ()
+        if (answerInt == questions[cur].answer)
+        {
+            StartCoroutine(NextQuestion());
+        }
+        else if (answerInt != questions[cur].answer)
+        {
+            Debug.Log("qqwee");
+        }
     }
 
+    public void _Start()
+    {
+        cur = Random.Range(0, questions.Length);
+
+        hasStarted = true;
+
+        introPop.SetActive(false);
+    }
+
+    void EndPop()
+    {
+        hasStarted = false;
+        endPop.SetActive(true);
+    }
+
+    IEnumerator NextQuestion()
+    {
+        yayPop.SetActive(true);
+        yayPop.GetComponent<Animation>().Play("MoneyYayPop");
+
+        yield return new WaitForSeconds(0.8f);
+
+        score += 1;
+        yayPop.SetActive(false);
+        cur = Random.Range(0, questions.Length);
+    }
+
+    public void _Back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void _Restart()
+    {
+        SceneManager.LoadScene("Y4 - Time Game");
+    }
 
     void Update()
     {
         if (hasStarted)
         {
-            // do stuff here
+            scoreText.text = score.ToString();
+            questionsText.text = questions[cur].dialogue;
+
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            timer = 0;
+            EndPop();
         }
 
         int minutes = Mathf.FloorToInt(timer / 60F);
